8c312b8d4a8d3127c7721cfea65c6cd2250a2c7d
thompsonpa <thompsonpa2014@gmail.com>
2020-01-03 08:54:11 -0500

    Initial MVC Code Push

---
+++ Duo-Web-Two-Factor-Authentication/Duo-Web-Two-Factor-Authentication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Duo_Web_Two_Factor_Authentication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Duo-Web-Two-Factor-Authentication/Duo-Web-Two-Factor-Authentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Duo_Web_Two_Factor_Authentication.MvcApplication" Language="C#" %>
